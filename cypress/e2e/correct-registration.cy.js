65d0036ae328df52416f44ebf97cbc0169734b2d
Kaline Dutra <kalineduttra@gmail.com>
2024-08-19 16:28:05 -0300

    refactor: divide testing responsibilities

--- cypress/e2e/correct-registration.cy.js
+++ cypress/e2e/correct-registration.cy.js
@@ -1,10 +1,14 @@
 describe('registration page', () => {
-  it('fill in the form fields correctly to register a new user', () => {
+  beforeEach(() =>{
     cy.visit('https://adopet-frontend-cypress.vercel.app/')
     cy.get('[data-test="register-button"]').click();
+  })
+
+  it('fill in the form fields correctly to register a new user', () => {
+    
     cy.get('[data-test="input-name"]').type('Julie');
     cy.get('[data-test="input-email"]').type('julie@email.com');
-    cy.get('[data-test="input-password"]').type('password123');
+    cy.get('[data-test="input-password"]').type('Password123');
     cy.get('[data-test="input-confirm-password"]').type('Password123');
     cy.get('[data-test="submit-button"]').click();
   })
