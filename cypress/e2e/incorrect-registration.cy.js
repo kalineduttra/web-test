2ea840125bf6768e17fb5a95238d7f310670294b
Kaline Dutra <kalineduttra@gmail.com>
2024-08-19 16:52:48 -0300

    test: adds test to sad path scenario

--- cypress/e2e/incorrect-registration.cy.js
+++ cypress/e2e/incorrect-registration.cy.js
@@ -1,7 +1,11 @@
 describe('registration page', () => {
-    it('filling in the user registration form incorrectly and displaying messages to the user ', () => {
-      cy.visit('https://adopet-frontend-cypress.vercel.app/')
+  beforeEach(() => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/')
       cy.get('[data-test="register-button"]').click();
+  })
+
+    it('filling in the user registration form incorrectly and displaying messages to the user ', () => {
+      
       cy.get('[data-test="submit-button"]').click();
       cy.contains('É necessário informar um endereço de email').should('be.visible');
         cy.contains('Crie uma senha').should('be.visible');
