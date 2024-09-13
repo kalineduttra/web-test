ed4a279664b411305d46b4fd52aab549a75f446c
Kaline Dutra <kalineduttra@gmail.com>
2024-09-13 15:57:23 -0300

    refactor:better separation of responsibility

--- cypress/e2e/correct-login.cy.js
+++ cypress/e2e/correct-login.cy.js
@@ -1,11 +1,10 @@
 describe('Login page', () => {
-    beforeEach(() => {
-      cy.visit('https://adopet-frontend-cypress.vercel.app/');
-      cy.get('[data-test="login-button"]').click();
-    })
-        it('Fill in the login fields with valid data and authenticate the user on the page', () => {
-          cy.get('[data-test="input-loginEmail"]').type('julie@email.com');
-          cy.get('[data-test="input-loginPassword"]').type('Password123');
-          cy.get('[data-test="submit-button"]').click();
-        })
-})
+  beforeEach(() => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('[data-test="login-button"]').click();
+  })
+    it('Fill in the login fields with valid data and authenticate the user on the page', () => {
+      cy.login('julie@email.com','Password123')
+      })
+  })
+
