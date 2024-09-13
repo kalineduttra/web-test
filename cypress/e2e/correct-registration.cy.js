81bc7cc0ee627edd47b1d1b3cb3939ce1e4db26e
Kaline Dutra <kalineduttra@gmail.com>
2024-09-13 15:58:44 -0300

    refactor:better separation of responsibility

--- cypress/e2e/correct-registration.cy.js
+++ cypress/e2e/correct-registration.cy.js
@@ -5,11 +5,6 @@ describe('registration page', () => {
   })
 
   it('fill in the form fields correctly to register a new user', () => {
-    
-    cy.get('[data-test="input-name"]').type('Julie');
-    cy.get('[data-test="input-email"]').type('julie@email.com');
-    cy.get('[data-test="input-password"]').type('Password123');
-    cy.get('[data-test="input-confirm-password"]').type('Password123');
-    cy.get('[data-test="submit-button"]').click();
+    cy.register('julie', 'julie@email.com', 'Password123')
   })
 })
