9bc7fe943c78c2e90fcecafecb65db4b7acbf9f7
Kaline Dutra <kalineduttra@gmail.com>
2024-11-03 23:54:43 -0300

    updt

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'rtntr4',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
