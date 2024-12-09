f3437bb472f0f030a39687bdcebf684fec47ee41
Kaline Dutra <kalineduttra@gmail.com>
2024-12-08 23:35:12 -0300

    refactory: add custom commands

--- cypress/e2e/GUI/registration-mass.cy.js
+++ cypress/e2e/GUI/registration-mass.cy.js
@@ -8,10 +8,6 @@ describe('Registration page', () => {
     users.forEach(user => {
         it ('Fill in the form fields correctly to register a user', () => {
             cy.register(user.name, user.email, user.password, user.password);
-            /*cy.get('[data-test="input-email"]').type(user.email);
-            cy.get('[data-test="input-password"]').type(user.password);
-            cy.get('[data-test="input-confirm-password"]').type(user.password);*/
-            cy.get('[data-test="submit-button"]').click();
         })
     })
 })
