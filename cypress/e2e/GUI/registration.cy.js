b8793a98e50a4ce13bc55dd6f580d7be63a74bc4
Kaline Dutra <kalineduttra@gmail.com>
2024-12-24 13:35:22 -0300

    changing the context of scenarios with bdd

--- cypress/e2e/GUI/registration.cy.js
+++ cypress/e2e/GUI/registration.cy.js
@@ -1,13 +1,13 @@
-describe('registration page', () => {
+describe('Given that I register an account', () => {
 
-  context('correct registration', () => {
+  context('When the fields are filled in correctly', () => {
     beforeEach(() => {
       cy.clearAllCookies
     })
-    it('fill in the form fields correctly', () => {
+    it('Then you should be redirected to the login screen', () => {
       cy.register(Cypress.env('validName'), Cypress.env('validEmail'),Cypress.env('validPassword'), Cypress.env('validPassword'))
     })
-    it('fill in password field with 14 characters', () => {
+    it('Then, when you fill in the 14-character field, you should register successfully', () => {
       cy.register(Cypress.env('validName'), Cypress.env('validEmail'),Cypress.env('password14Characters'), Cypress.env('password14Characters'))
     })
     afterEach(() => {
@@ -16,27 +16,27 @@ describe('registration page', () => {
     })
   })
 
-  context('When I register incorrectly', () => {
+  context.only('When you fill in the form fields incorrectly', () => {
     const passwordRequirements = "A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres"
-    it('create account without filling in name', () => {
+    it('Then when you fill in the form without the name you should ask them to fill in this field', () => {
       cy.register(' ', Cypress.env('validEmail'),Cypress.env('validPassword'), Cypress.env('validPassword'))
     })    
-    it('* fill in the name field incorrectly', () => {
+    it('Then when you fill in the form with the wrong name you should be asked to fill it in correctly', () => {
       cy.register(Cypress.env('invalidName'), Cypress.env('validEmail'),Cypress.env('validPassword'), Cypress.env('validPassword'))
     })
-    it('fill in the email field incorrectly', () => {
+    it('Then it must fill in the form with the wrong email and displays a message to adjust it', () => {
       cy.register(Cypress.env('validName'), Cypress.env('invalidEmail'),Cypress.env('validPassword'), Cypress.env('validPassword'))
       cy.contains('Por favor, verifique o email digitado').should('be.visible')
     })
-    it('password with lower characters', () => {
+    it('Then must fill in the password field with a character limit below the requirement should display message to adjust', () => {
       cy.register(Cypress.env('validName'), Cypress.env('validEmail'),Cypress.env('invalidPassword'), Cypress.env('invalidPassword'))
       cy.contains(passwordRequirements).should('be.visible')
     })
-    it('fill in password confirmation with content that differs from the password', () => {
+    it('Then when filling in the confirmation password which is different from the password it should display a warning message that they are not the same', () => {
       cy.register(Cypress.env('validName'), Cypress.env('validEmail'),Cypress.env('validPassword'), Cypress.env('invalidPassword'))
       cy.contains('As senhas não batem').should('be.visible')
     })
-    it('password with exceeded characters', () => {
+    it('Then it should display a password setting message when filling in the password field longer than 14 characters', () => {
       cy.register(Cypress.env('validName'), Cypress.env('validEmail'),Cypress.env('passwordExceededCharacters'), Cypress.env('passwordExceededCharacters'))
       cy.contains(passwordRequirements).should('be.visible')
     })
