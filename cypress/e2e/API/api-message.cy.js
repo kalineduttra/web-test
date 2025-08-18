99c66be2ee1bccb78dad5c209294be561ff54035
Kaline <123281959+kalineduttra@users.noreply.github.com>
2025-08-18 09:51:51 -0300

    feat(api): add tests for message endpoint

--- cypress/e2e/API/api-message.cy.js
+++ cypress/e2e/API/api-message.cy.js
@@ -1,9 +1,16 @@
+const { validEmail, validPassword } = Cypress.env()
+
 describe('Api Adopet', () => {
+    beforeEach(() => {
+        cy.loginApi(validEmail, validPassword)
+    })
     it ('Api message', () => {
         cy.request({
             method: 'GET',
-            url: 'https://adopet-api-i8qu.onrender.com/mensagem/2b8ba748-1c73-4038-bd21-e859d991d562',
-            headers: Cypress.env()
+            url: 'https://adopet-api-i8qu.onrender.com/mensagem/26fd6a23-8741-48b9-9d0a-6ff4c4c001fb',
+            headers: {
+                Authorization: `Bearer ${Cypress.env('AUTH_TOKEN')}`
+            }
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
