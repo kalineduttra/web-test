42e76d0b4002530266a1e29285d506c43f3d5224
Kaline <123281959+kalineduttra@users.noreply.github.com>
2025-08-18 09:54:24 -0300

    refactor(login-api): optimize profile test and data validation

--- cypress/e2e/API/login-profile-api.cy.js
+++ cypress/e2e/API/login-profile-api.cy.js
@@ -1,14 +1,17 @@
+const {validEmail, validPassword, validName} = Cypress.env()
+
 describe('Api Adopet', () => {
     it('Verify username is present on /profile page response', () => {
         cy.request({
             method: 'GET',
-            url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/2b8ba748-1c73-4038-bd21-e859d991d562',
+            url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/26fd6a23-8741-48b9-9d0a-6ff4c4c001fb',
             headers: Cypress.env()
-        }).then ((res) => {
+        }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
             expect(res.body).to.have.property('perfil')
-            expect(res.body.perfil.nome).to.be.equal('monica')
+            expect(res.body.perfil.nome).to.be.equal(validName)
         })
     })
 })
+
