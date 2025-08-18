f8f6f2d446af2e2c37aef365e73ef30a71910af3
Kaline <123281959+kalineduttra@users.noreply.github.com>
2025-08-18 09:53:04 -0300

    refactor(login-api): handle login failure with mocked API response

--- cypress/e2e/API/incorrect-login.cy.js
+++ cypress/e2e/API/incorrect-login.cy.js
@@ -1,20 +1,13 @@
+const {validEmail, validPassword, invalidEmail, invalidPassword, incorrectPassword } = Cypress.env()
+
 describe('Login page', () => {
   beforeEach(() => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/');
-    cy.get('[data-test="login-button"]').click();
-    cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {
-      statusCode:400, }).as('stunPost')
-  })
+    cy.visit('/');
+    cy.get('[data-test="login-button"]').click()
+    cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {statusCode:400}).as('stubPost')})
     
-      it('Should fill in login fields incorrectly and display error messages to the user on the page', () => {
-        cy.get('[data-test="input-loginEmail"]').type('Joker3');
-        cy.get('[data-test="input-loginPassword"]').type('123');
-        cy.get('[data-test="submit-button"]').click();
-        cy.contains('Por favor, verifique o email digitado').should('be.visible')
-        cy.contains('A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres').should('be.visible')
-      })
       it('should fail even if the fields are filled in correctly', () => {
-        cy.login('email@email.com','Password1234')
+        cy.signIn(validEmail, incorrectPassword)
         cy.wait('@stubPost')
         cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible')
       })
